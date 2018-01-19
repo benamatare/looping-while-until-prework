2ebeb0bf4b08ee1c908dc08102c7fe765931234c
Ben Matare <ben.a.matare@gmail.com>
2018-01-19 04:08:21 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,17 @@
 
 
 def using_until
+<<<<<<< HEAD
   #counter
   levitation_force = 6
 until levitation_force == 10
   puts "Wingardium Leviosa"
   levitation_force += 1
 end
+=======
+        levitation_force = 6
+  until levitation_force == 6
+    puts "Wingardium Leviosa"      #your code here
+  end
+>>>>>>> 0dd72936bcc7d4782d53f834cb4d961528cc9cf0
 end
--- lib/while.rb
+++ lib/while.rb
@@ -3,8 +3,16 @@
 def using_while
 	#counter
 	levitation_force = 6
+<<<<<<< HEAD
 while levitation_force < 10
 	puts "Wingardium Leviosa"
 	levitation_force += 1
 end
 end
+=======
+	while using_while > 10
+		puts " Wingardium Leviosa"
+	#your code here
+end
+end
+>>>>>>> 0dd72936bcc7d4782d53f834cb4d961528cc9cf0
